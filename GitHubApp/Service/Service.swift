b2d76266b9a973d13fb2fff1190792f9f6ca7169
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2023-04-27 23:59:27 -0300

    feat: fetch list service implementation

--- GitHubApp/Service/Service.swift
+++ GitHubApp/Service/Service.swift
@@ -9,7 +9,27 @@ struct Service {
     }
 
     func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
-        
-        // TODO
+        let url = URL(string: "https://api.github.com/users/\(user)/repos")
+
+        guard let url = url else { return }
+        network.performGet(url: url) { data in
+            if let convertedData = self.convertDataIntoRepositories(data: data) {
+                completion(convertedData)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+    
+    func convertDataIntoRepositories(data: Data?) -> [Repository]? {
+        guard let data = data else { return nil }
+        do {
+            let decoder = JSONDecoder()
+            let repositories = try decoder.decode([Repository].self, from: data)
+            return repositories
+        } catch let error {
+            print("Error: \(error.localizedDescription)")
+            return nil
+        }
     }
 }
